046e1bcd036d50cef7fdc3c9d296ee146d7fa9c5
Jaber-Ali <Jaber.Ali@se.experis.com>
2021-08-24 10:08:11 +0200

    added components- login, pokemons, trainer and dataservice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { TrainerComponent } from './trainer/trainer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PokemonsComponent,
+    TrainerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    //Http
   ],
   providers: [],
   bootstrap: [AppComponent]
